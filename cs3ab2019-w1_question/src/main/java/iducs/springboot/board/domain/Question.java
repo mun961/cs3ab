61e004e80d7d072bce2b31411fdf5125a66b676d
mun961 <mun961@naver.com>
2019-06-19 20:45:20 +0900

    git test

---
+++ cs3ab2019-w1_question/src/main/java/iducs/springboot/board/domain/Question.java
@@ -0,0 +1,79 @@
+package iducs.springboot.board.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Question {
+	public Question(long id, String title, User writer, String contents) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.writer = writer;
+		this.contents = contents;
+		this.createTime = LocalDateTime.now();
+	}
+
+	private long id; // primary key
+	private String title; 
+	private User writer;	
+	private List<Answer> answers;
+
+	private String contents;
+	private LocalDateTime createTime;	
+	
+	public Question() {}
+	public Question(String title, User writer, String contents) {
+		super();
+		this.title = title;
+		this.writer = writer;
+		this.contents = contents;
+		this.createTime = LocalDateTime.now();
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public User getWriter() {
+		return writer;
+	}
+
+	public void setWriter(User writer) {
+		this.writer = writer;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public LocalDateTime getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(LocalDateTime createTime) {
+		this.createTime = createTime;
+	}
+	
+}
